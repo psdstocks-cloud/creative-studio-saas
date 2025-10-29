368ada94eaa8d8243c91dd78d8f2d2ce1ad46696
PSD <psdstockss@gmail.com>
2025-10-30 00:31:24 +0300

    fix: Add x-request-id to CORS allowed headers
    - Add x-request-id to Access-Control-Allow-Headers
    - Fixes CORS preflight error blocking BFF session requests
    - Frontend Axios interceptor adds this header for request tracking

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-request-id');
   }
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
