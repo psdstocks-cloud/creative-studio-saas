f3d641c4581534527ac648ddb23b01e500604d96
PSD <psdstockss@gmail.com>
2025-10-30 15:43:06 +0300

    fix: icon files for admin page

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -276,3 +276,19 @@ export const ChevronLeftIcon = ({ className }: IconProps) => (
     <polyline points="15 6 9 12 15 18" />
   </svg>
 );
+
+export const CheckIcon = ({ className }: IconProps) => (
+  <svg {...iconProps} className={className || iconProps.className} viewBox="0 0 24 24">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <polyline points="20 6 9 17 4 12" />
+  </svg>
+);
+
+export const PencilSquareIcon = ({ className }: IconProps) => (
+  <svg {...iconProps} className={className || iconProps.className} viewBox="0 0 24 24">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+    <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+    <line x1="16" y1="5" x2="19" y2="8" />
+  </svg>
+);
